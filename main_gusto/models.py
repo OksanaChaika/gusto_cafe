9136e9a98ff811d0441dc9484268ea39529435da
Admin <oksana.chaika89@gmail.com>
2021-02-16 20:58:43 +0200

    16.02 (Tu)
    Signed-off-by: Admin <oksana.chaika89@gmail.com>

--- main_gusto/models.py
+++ main_gusto/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from uuid import uuid4
+from os import path
+
+
+def get_file_name(filename):
+    ext = filename.strip().split('.')[-1]
+    filename = f'{uuid4()}.{ext}'
+    return path.join('media/images/categories', filename)
+
+
 
 # Create your models here.
+class Category(models.Model):
+
+
+    title = models.CharField(max_length=15, unique=True)
+    photo = models.ImageField(upload_to=get_file_name)
+    category_order = models.IntegerField(unique=True)
+    is_visible = models.BooleanField(default=True)
+
+
+    def __str__(self):
+        return f'{self.title}:{self.category_order}'
+
+
