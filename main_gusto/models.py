b5df51021f6aeb1f493046b2b46a11e94d778de7
Admin <oksana.chaika89@gmail.com>
2021-02-18 21:09:50 +0200

    18.02 (Th)
    Signed-off-by: Admin <oksana.chaika89@gmail.com>

--- main_gusto/models.py
+++ main_gusto/models.py
@@ -3,12 +3,15 @@ from uuid import uuid4
 from os import path
 
 
-def get_file_name(filename):
+def get_file_name(self, filename):
     ext = filename.strip().split('.')[-1]
     filename = f'{uuid4()}.{ext}'
     return path.join('media/images/categories', filename)
 
-
+def get_file_name2(self, filename):
+    ext = filename.strip().split('.')[-1]
+    filename = f'{uuid4()}.{ext}'
+    return path.join('media/images/dishes', filename)
 
 # Create your models here.
 class Category(models.Model):
@@ -23,4 +26,16 @@ class Category(models.Model):
     def __str__(self):
         return f'{self.title}:{self.category_order}'
 
+
+class Dish(models.Model):
+    title = models.CharField(max_length=25, unique=True)
+    photo = models.ImageField(upload_to=get_file_name2)
+    dish_order = models.IntegerField()
+    is_visible = models.BooleanField(default=True)
+    price = models.DecimalField(max_digits=6,decimal_places=2)
+    desc = models.CharField(max_length=150, unique=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.title}: {self.price}'
 
--- main_gusto/views.py
+++ main_gusto/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
+from .models import Category, Dish
 
 # Create your views here.
 def main_page_view(request):
-    categories = [
-        'Закуски',
-        'Основные блюда',
-        'Гарниры',
-        'Десерты'
-    ]
+    categories = Category.objects.filter(is_visible=True).order_by('category_order')
+    for item in categories:
+        dishes = Dish.objects.filter(category=item.pk).filter(is_visible=True).order_by('dish_order')
+        item.dishes = dishes
+
+    special = Dish.objects.filter(category__title='Особенное меню')
+
 
     return render(request, 'index.html', context={
-        'categories': categories
+        'categories': categories,
+        'special': special
     })
