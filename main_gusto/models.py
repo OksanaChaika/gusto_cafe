ef10a00f4df3a805dba07bc54e2cdc7afe058ffc
Admin <oksana.chaika89@gmail.com>
2021-02-23 21:06:48 +0200

    23.02 (Tu)
    Signed-off-by: Admin <oksana.chaika89@gmail.com>

--- main_gusto/models.py
+++ main_gusto/models.py
@@ -13,15 +13,22 @@ def get_file_name2(self, filename):
     filename = f'{uuid4()}.{ext}'
     return path.join('media/images/dishes', filename)
 
+def get_file_name3(self, filename):
+    ext = filename.strip().split('.')[-1]
+    filename = f'{uuid4()}.{ext}'
+    return path.join('media/images/info', filename)
+
+def get_file_name4(self, filename):
+    ext = filename.strip().split('.')[-1]
+    filename = f'{uuid4()}.{ext}'
+    return path.join('media/images/team', filename)
+
 # Create your models here.
 class Category(models.Model):
-
-
     title = models.CharField(max_length=15, unique=True)
     photo = models.ImageField(upload_to=get_file_name)
     category_order = models.IntegerField(unique=True)
     is_visible = models.BooleanField(default=True)
-
 
     def __str__(self):
         return f'{self.title}:{self.category_order}'
@@ -38,4 +45,59 @@ class Dish(models.Model):
 
     def __str__(self):
         return f'{self.title}: {self.price}'
+
+
+class Info(models.Model):
+    cafe_info = models.CharField(max_length=500, unique=True)
+    photo = models.ImageField(upload_to=get_file_name3)
+    is_visible = models.BooleanField(default=True)
+
+
+class Team(models.Model):
+    team_info = models.CharField(max_length=500, unique=True)
+    photo = models.ImageField(upload_to=get_file_name4)
+    is_visible = models.BooleanField(default=True)
+
+
+class Phone(models.Model):
+    phone = models.CharField(max_length=13, unique=True)
+    is_visible = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f'{self.phone}'
+
+class Address(models.Model):
+    city = models.CharField(max_length=20)
+    street = models.CharField(max_length=20)
+    home = models.CharField(max_length=5)
+    is_visible = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f'{self.city}; {self.street}; {self.home}'
+
+
+class OpeningHours(models.Model):
+    day = models.CharField(max_length=10)
+    hours_start = models.CharField(max_length=5)
+    hours_end = models.CharField(max_length=5)
+
+    def __str__(self):
+        return f'{self.day} - {self.hours_start} - {self.hours_end}'
+
+class CafeInfo(models.Model):
+    address_id = models.ForeignKey(Address, on_delete=models.CASCADE)
+    phone_id = models.ForeignKey(Phone, on_delete=models.CASCADE)
+
+
+class Message(models.Model):
+    user_name = models.CharField(max_length=40)
+    user_email = models.EmailField()
+    user_message = models.CharField(max_length=400)
+
+    pub_date = models.DateField(auto_now_add=True)
+    is_processed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.user_name}:{self.user_email}:{self.user_message[:20]}'
+
 
--- main_gusto/views.py
+++ main_gusto/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from .models import Category, Dish
+from .models import *
 
 # Create your views here.
 def main_page_view(request):
+    team_info = Team.objects.all().filter(is_visible=True)
     categories = Category.objects.filter(is_visible=True).order_by('category_order')
     for item in categories:
         dishes = Dish.objects.filter(category=item.pk).filter(is_visible=True).order_by('dish_order')
@@ -10,8 +11,12 @@ def main_page_view(request):
 
     special = Dish.objects.filter(category__title='Особенное меню')
 
-
     return render(request, 'index.html', context={
         'categories': categories,
-        'special': special
+        'special': special,
+        'team_info': team_info[0]
     })
+
+def dish_page_view(request, pk):
+    dish = Dish.objects.get(pk=pk)
+    return render(request, 'dish_info.html', context={'dish': dish})
